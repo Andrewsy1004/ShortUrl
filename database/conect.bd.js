3baf76fdfe9d95be79bd302fe9e272e0b066b635
Andrewsy1004 <136858623+Andrewsy1004@users.noreply.github.com>
2023-12-03 11:52:32 +0000

    Delete database directory

--- database/conect.bd.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-try {
-    await mongoose.connect(process.env.URL_MONGO);
-    console.log("db conectada");
-} catch (error) {
-    console.log("Error!! " + error);
-}
